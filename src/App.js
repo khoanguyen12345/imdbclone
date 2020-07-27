36666b767f90149e76f5e3c47ce0e83fdb2b153e
khoanguyen12345 <67582698+khoanguyen12345@users.noreply.github.com>
2020-07-27 07:04:20 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useCallback} from 'react';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -9,14 +9,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 function App(url) {
- 
+  let searchString=''
   let [movieList,setMovieList] = useState(null)
   let [page,setPage] = useState(1)
   let [totalPages,setTotalPages] = useState(30)
   let [urlState,setUrlState] = useState("movie/now_playing")
-  let [title,setSearchTitle] = useState(null)
+  let [title,setSearchTitle] = useState("")
 const callMovie = async()=>{
   let url = `https://api.themoviedb.org/3/${urlState}?api_key=7df18e01feebb6c6eec04a10e111222f&language=en-US&page=${page}${title}`
+  console.log(url)
   let result = await fetch (url)
   let data = await result.json()
   console.log("DATA", data)
@@ -26,6 +27,7 @@ const callMovie = async()=>{
 useEffect(()=>{
   const callMovie = async()=>{
     let url = `https://api.themoviedb.org/3/${urlState}?api_key=7df18e01feebb6c6eec04a10e111222f&language=en-US&page=${page}${title}`
+    console.log(url)
     let result = await fetch (url)
     let data = await result.json()
     console.log("DATA", data)
@@ -33,7 +35,7 @@ useEffect(()=>{
     setMovieList(data.results)
   }
   callMovie()
-},[page,title,urlState])
+},[urlState,page,title])
 
   if (movieList==null){
     return( 
@@ -53,15 +55,15 @@ return (
              <Nav.Link onClick = {()=>{setUrlState("movie/upcoming");setSearchTitle(null);callMovie();}}>Upcoming</Nav.Link>
              </Nav>
              <Form inline>
-            <FormControl onChange={e => {setUrlState(`search/movie`);setSearchTitle(`&query=${e.target.value}`)}} type="text" placeholder="Search" className="mr-sm-2" />
-             <Button onClick ={()=>{callMovie();}} variant="outline-info">Search</Button>
+            <FormControl onChange={e => {searchString=e.target.value;}} type="text" placeholder="Search" className="mr-sm-2" />
+             <Button onClick ={()=>{{setUrlState(`search/movie`);setSearchTitle(`&query=${searchString}`)}callMovie();}} variant="outline-info">Search</Button>
              </Form>
              </Navbar>
           <div>
           <MovieBoard movieList = {movieList}/>
           </div>
           <div className = "footer">
-          <Button className = "seeMoreButton" onClick = {()=> {setPage(page+1);callMovie();if(page===totalPages){setPage(1)};}} variant="primary" size="lg" block>
+          <Button className = "seeMoreButton" onClick = {()=> {setPage(page+1);console.log(page);if(page===totalPages){setPage(1)};callMovie();}} variant="primary" size="lg" block>
             See More</Button>
           </div>
     </div>
